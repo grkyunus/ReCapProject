3515d45abf3c463280bc28ae37d3072ec8023417
Gırık <yunus.yk2000@gmail.com>
2023-05-19 18:21:17 +0300

    Image kontrolleri yapılmadı. Autofac ımage eklendi.

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -39,6 +39,9 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
             builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
 
+            builder.RegisterType<ImageManager>().As<IImageService>().SingleInstance();
+            builder.RegisterType<EfImageDal>().As<IImageDal>().SingleInstance();
+
             #endregion
 
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
